c404f07eb0ea27c46f20ca2178b5808636e98655
martincostello <martin@martincostello.com>
2024-07-29 17:29:45 +0100

    Increase test coverage
    Increase test coverage.

---
+++ tests/OpenApi.Extensions.Tests/OpenApiEndpointConventionBuilderExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Builder;
+
+namespace MartinCostello.OpenApi;
+
+public static class OpenApiEndpointConventionBuilderExtensionsTests
+{
+    [Fact]
+    public static void ProducesOpenApiResponse_Throws_If_Builder_Is_Null()
+    {
+        // Arrange
+        IEndpointConventionBuilder builder = null!;
+
+        // Act and Assert
+        Should.Throw<ArgumentNullException>(() => builder.ProducesOpenApiResponse(200, "OK"));
+    }
+}
---
+++ tests/OpenApi.Extensions.Tests/OpenApiResponseAttributeTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.OpenApi;
+
+public static class OpenApiResponseAttributeTests
+{
+    [Fact]
+    public static void OpenApiResponseAttribute_Constructor_Initializes_Instance()
+    {
+        // Arrange
+        var statusCode = 200;
+        var description = "OK";
+
+        // Act
+        var actual = new OpenApiResponseAttribute(statusCode, description);
+
+        // Assert
+        actual.HttpStatusCode.ShouldBe(statusCode);
+        actual.Description.ShouldBe(description);
+    }
+}
