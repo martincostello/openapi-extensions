479856ec7c032aad58505c456d13eda05f5c4987
martincostello <martin@martincostello.com>
2024-07-29 09:03:08 +0100

    Refactor tests
    Refactor the tests to allow for more to be added later with different dynamic endpoint definitions.

--- tests/OpenApi.Extensions.Tests/DocumentTests.cs
+++ tests/OpenApi.Extensions.Tests/DocumentTests.cs
@@ -1,11 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MartinCostello.OpenApi;
 
@@ -17,32 +13,23 @@ public class DocumentTests(ITestOutputHelper outputHelper)
     public async Task Schema_Is_Correct()
     {
         // Arrange
-        using var fixture = new WebApplicationFactory<Program>()
-            .WithWebHostBuilder((builder) =>
+        using var fixture = new TestFixture(
+            (services) =>
             {
-                builder.ConfigureLogging(
-                    (logging) =>
-                        logging.ClearProviders()
-                               .AddXUnit(outputHelper)
-                               .SetMinimumLevel(LogLevel.Information));
-
-                builder.ConfigureServices((services) =>
+                services.AddHttpContextAccessor();
+                services.AddOpenApiExtensions((options) =>
                 {
-                    services.AddHttpContextAccessor();
-                    services.AddOpenApiExtensions((options) =>
-                    {
-                        options.AddExamples = true;
-                        options.AddServerUrls = true;
-                        options.AddXmlComments<Greeting>();
-                        options.SerializationContext = AppJsonSerializationContext.Default;
-                    });
+                    options.AddExamples = true;
+                    options.AddServerUrls = true;
+                    options.AddXmlComments<Greeting>();
+                    options.SerializationContext = AppJsonSerializationContext.Default;
                 });
-            });
-
-        using var client = fixture.CreateDefaultClient();
+            },
+            (endpoints) => { },
+            outputHelper);
 
         // Act
-        var actual = await client.GetStringAsync("/openapi/v1.json");
+        var actual = await fixture.GetOpenApiDocumentAsync();
 
         // Assert
         await Verifier.VerifyJson(actual, Settings);
---
+++ tests/OpenApi.Extensions.Tests/TestFixture.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MartinCostello.OpenApi;
+
+public sealed class TestFixture(
+    Action<IServiceCollection> configureServices,
+    Action<IEndpointRouteBuilder> configureEndpoints,
+    ITestOutputHelper outputHelper) : WebApplicationFactory<Program>()
+{
+    public async Task<string> GetOpenApiDocumentAsync()
+    {
+        using var client = CreateDefaultClient();
+        return await client.GetStringAsync("/openapi/v1.json");
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureLogging((builder) =>
+        {
+            builder.ClearProviders()
+                   .AddXUnit(outputHelper)
+                   .SetMinimumLevel(LogLevel.Information);
+        });
+
+        builder.ConfigureServices((services) =>
+        {
+            services.AddSingleton<IStartupFilter>(ConfigureEndpointsFilter.Create(configureEndpoints));
+            configureServices(services);
+        });
+    }
+
+    private sealed class ConfigureEndpointsFilter(Action<IEndpointRouteBuilder> configure) : IStartupFilter
+    {
+#pragma warning disable CA1859
+        public static IStartupFilter Create(Action<IEndpointRouteBuilder> configure)
+            => new ConfigureEndpointsFilter(configure);
+#pragma warning restore CA1859
+
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return (builder) =>
+            {
+                next(builder);
+                builder.UseEndpoints(configure);
+            };
+        }
+    }
+}
