37eefc5ee63ecc35344ff212255607031bea2357
martincostello <martin@martincostello.com>
2024-07-29 17:29:45 +0100

    Increase test coverage
    Increase test coverage.

---
+++ tests/OpenApi.Extensions.Tests/OpenApiExampleAttributeTests.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Writers;
+
+namespace MartinCostello.OpenApi;
+
+public static partial class OpenApiExampleAttributeTests
+{
+    [Fact]
+    public static void OpenApiExampleAttribute_Constructor_Initializes_Instance()
+    {
+        // Arrange
+        var value = "example";
+
+        // Act
+        var actual = new OpenApiExampleAttribute(value);
+
+        // Assert
+        actual.Value.ShouldBe(value);
+        actual.GenerateExample().ShouldBe(value);
+    }
+
+    [Fact]
+    public static void OpenApiExampleAttribute_IExampleProvider_Implementation_Is_Correct()
+    {
+        // Act
+        var actual = GetExample<string, OpenApiExampleAttribute>();
+
+        // Assert
+        actual.ShouldBe("value");
+    }
+
+    [Fact]
+    public static void OpenApiExampleAttributeT1_Constructor_Initializes_Instance()
+    {
+        // Act
+        var actual = new OpenApiExampleAttribute<Human>();
+
+        // Assert
+        actual.ExampleType.ShouldBe(typeof(Human));
+    }
+
+    [Fact]
+    public static void OpenApiExampleAttributeT1_GenerateExample_Returns_Correct_Value()
+    {
+        // Arrange
+        var target = new OpenApiExampleAttribute<Human>();
+
+        // Act
+        var actual = target.GenerateExample();
+
+        // Assert
+        actual.ShouldNotBeNull();
+        actual.Name.ShouldBe("Martin");
+    }
+
+    [Fact]
+    public static void OpenApiExampleAttributeT2_Constructor_Initializes_Instance()
+    {
+        // Act
+        var actual = new OpenApiExampleAttribute<Person, PersonExampleProvider>();
+
+        // Assert
+        actual.ExampleType.ShouldBe(typeof(Person));
+    }
+
+    [Fact]
+    public static void OpenApiExampleAttributeT2_GenerateExample_Returns_Correct_Value()
+    {
+        // Arrange
+        var target = new OpenApiExampleAttribute<Person, PersonExampleProvider>();
+
+        // Act
+        var actual = target.GenerateExample();
+
+        // Assert
+        actual.ShouldNotBeNull();
+        actual.Name.ShouldBe("Martin");
+    }
+
+    [Theory]
+    [InlineData(false, "Name")]
+    [InlineData(true, "name")]
+    public static void OpenApiExampleAttributeT2_GenerateExample_As_IOpenApiExampleMetadata_Returns_Correct_Value(
+        bool camelCase,
+        string expectedPropertyName)
+    {
+        // Arrange
+        IOpenApiExampleMetadata target = new OpenApiExampleAttribute<Person, PersonExampleProvider>();
+        JsonSerializerContext context = camelCase ? PersonJsonSerializationContextCamel.Default : PersonJsonSerializationContextPascal.Default;
+
+        // Act
+        var actual = target.GenerateExample(context);
+
+        // Assert
+        actual.ShouldNotBeNull();
+
+        // Arrange
+        var stringWriter = new StringWriter();
+        var jsonWriter = new OpenApiJsonWriter(stringWriter);
+
+        // Act
+        actual.Write(jsonWriter, Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0);
+
+        // Assert
+        using var document = JsonDocument.Parse(stringWriter.ToString());
+
+        document.ShouldNotBeNull();
+        document.RootElement.ValueKind.ShouldBe(JsonValueKind.Object);
+        document.RootElement.EnumerateObject().Count().ShouldBe(1);
+        document.RootElement.TryGetProperty(expectedPropertyName, out var value).ShouldBeTrue();
+
+        value.ValueKind.ShouldBe(JsonValueKind.String);
+        value.GetString().ShouldBe("Martin");
+    }
+
+    private static TSchema GetExample<TSchema, TProvider>()
+        where TProvider : IExampleProvider<TSchema>
+        => TProvider.GenerateExample();
+
+    private sealed record Human(string Name) : IExampleProvider<Human>
+    {
+        public static Human GenerateExample() => new("Martin");
+    }
+
+    private sealed record Person(string Name);
+
+    private sealed class PersonExampleProvider : IExampleProvider<Person>
+    {
+        public static Person GenerateExample() => new("Martin");
+    }
+
+    [JsonSerializable(typeof(Person))]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+    private sealed partial class PersonJsonSerializationContextCamel : JsonSerializerContext;
+
+    [JsonSerializable(typeof(Person))]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.Unspecified)]
+    private sealed partial class PersonJsonSerializationContextPascal : JsonSerializerContext;
+}
---
+++ tests/OpenApi.Extensions.Tests/OpenApiExtensionsTests.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace MartinCostello.OpenApi;
+
+public static class OpenApiExtensionsTests
+{
+    [Fact]
+    public static void AddOpenApiExtensions_Validates_Arguments()
+    {
+        // Arrange
+        string documentName = "api";
+
+        IServiceCollection services = null!;
+
+        // Act and Assert
+        Should.Throw<ArgumentNullException>(services.AddOpenApiExtensions).ParamName.ShouldBe("services");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(documentName)).ParamName.ShouldBe("services");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(documentName, ConfigureOneService)).ParamName.ShouldBe("services");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(documentName, ConfigureTwoServices)).ParamName.ShouldBe("services");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(ConfigureOneService)).ParamName.ShouldBe("services");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(ConfigureTwoServices)).ParamName.ShouldBe("services");
+
+        // Arrange
+        services = new ServiceCollection();
+        Action<OpenApiExtensionsOptions> configureOne = null!;
+        Action<OpenApiOptions, OpenApiExtensionsOptions> configureTwo = null!;
+
+        // Act and Assert
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(configureOne)).ParamName.ShouldBe("configureOptions");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(configureTwo)).ParamName.ShouldBe("configureOptions");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(documentName, configureOne)).ParamName.ShouldBe("configureOptions");
+        Should.Throw<ArgumentNullException>(() => services.AddOpenApiExtensions(documentName, configureTwo)).ParamName.ShouldBe("configureOptions");
+
+        static void ConfigureOneService(OpenApiExtensionsOptions options)
+            => options.ShouldNotBeNull();
+
+        static void ConfigureTwoServices(OpenApiOptions first, OpenApiExtensionsOptions second)
+        {
+            first.ShouldNotBeNull();
+            second.ShouldNotBeNull();
+        }
+    }
+
+    [Fact]
+    public static void AddOpenApiExtensions_Throws_If_Examples_Enabled_But_Not_JsonSerializerContext_Specified()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        services.AddOpenApi();
+        services.AddOpenApiExtensions((options) =>
+        {
+            options.AddExamples = true;
+            options.SerializationContext = null;
+        });
+
+        using var serviceProvider = services.BuildServiceProvider();
+        var monitor = serviceProvider.GetRequiredService<IOptionsMonitor<OpenApiOptions>>();
+
+        // Act and Assert
+        var exception = Should.Throw<InvalidOperationException>(() => monitor.Get("v1"));
+        exception.Message.ShouldBe(@"No JsonSerializerContext has been configured on the OpenApiExtensionsOptions instance for the OpenAPI document ""v1"".");
+    }
+}
