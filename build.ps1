7c86ccc497e5bbfa35d98fff329e5f4f0dd7c060
martincostello <martin@martincostello.com>
2024-08-07 14:37:51 +0100

    Refactor script
    Update some syntax.

--- build.ps1
+++ build.ps1
@@ -15,7 +15,7 @@ $sdkFile = Join-Path $solutionPath "global.json"
 
 $dotnetVersion = (Get-Content $sdkFile | Out-String | ConvertFrom-Json).sdk.version
 
-$installDotNetSdk = $false;
+$installDotNetSdk = $false
 
 if (($null -eq (Get-Command "dotnet" -ErrorAction SilentlyContinue)) -and ($null -eq (Get-Command "dotnet.exe" -ErrorAction SilentlyContinue))) {
     Write-Host "The .NET SDK is not installed."
@@ -35,26 +35,26 @@ else {
     }
 }
 
-if ($installDotNetSdk -eq $true) {
+if ($installDotNetSdk) {
 
     ${env:DOTNET_INSTALL_DIR} = Join-Path $PSScriptRoot ".dotnetcli"
     $sdkPath = Join-Path ${env:DOTNET_INSTALL_DIR} "sdk" $dotnetVersion
 
-    if (!(Test-Path $sdkPath)) {
-        if (!(Test-Path ${env:DOTNET_INSTALL_DIR})) {
+    if (-Not (Test-Path $sdkPath)) {
+        if (-Not (Test-Path ${env:DOTNET_INSTALL_DIR})) {
             mkdir ${env:DOTNET_INSTALL_DIR} | Out-Null
         }
         [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
-        if (($PSVersionTable.PSVersion.Major -ge 6) -And !$IsWindows) {
+        if (($PSVersionTable.PSVersion.Major -ge 6) -And (-Not $IsWindows)) {
             $installScript = Join-Path ${env:DOTNET_INSTALL_DIR} "install.sh"
             Invoke-WebRequest "https://dot.net/v1/dotnet-install.sh" -OutFile $installScript -UseBasicParsing
             chmod +x $installScript
-            & $installScript --version $dotnetVersion --install-dir ${env:DOTNET_INSTALL_DIR} --no-path --skip-non-versioned-files
+            & $installScript --jsonfile $sdkFile --install-dir ${env:DOTNET_INSTALL_DIR} --no-path --skip-non-versioned-files
         }
         else {
             $installScript = Join-Path ${env:DOTNET_INSTALL_DIR} "install.ps1"
             Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -OutFile $installScript -UseBasicParsing
-            & $installScript -Version $dotnetVersion -InstallDir ${env:DOTNET_INSTALL_DIR} -NoPath -SkipNonVersionedFiles
+            & $installScript -JsonFile $sdkFile -InstallDir ${env:DOTNET_INSTALL_DIR} -NoPath -SkipNonVersionedFiles
         }
     }
 }
@@ -64,7 +64,7 @@ else {
 
 $dotnet = Join-Path ${env:DOTNET_INSTALL_DIR} "dotnet"
 
-if ($installDotNetSdk -eq $true) {
+if ($installDotNetSdk) {
     ${env:PATH} = "${env:DOTNET_INSTALL_DIR};${env:PATH}"
 }
 
@@ -83,7 +83,7 @@ function DotNetTest {
 
     $additionalArgs = @()
 
-    if (![string]::IsNullOrEmpty(${env:GITHUB_SHA})) {
+    if (-Not [string]::IsNullOrEmpty(${env:GITHUB_SHA})) {
         $additionalArgs += "--logger"
         $additionalArgs += "GitHubActions;report-warnings=false"
     }
@@ -108,7 +108,7 @@ ForEach ($project in $packageProjects) {
     DotNetPack $project $Configuration
 }
 
-if ($SkipTests -eq $false) {
+if (-Not $SkipTests) {
     Write-Host "Testing $($testProjects.Count) project(s)..." -ForegroundColor Green
     ForEach ($project in $testProjects) {
         DotNetTest $project
