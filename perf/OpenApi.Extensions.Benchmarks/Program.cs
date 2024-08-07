c737cfb88b5f289fcaa6e4bd708d40a316458dbe
martincostello <martin@martincostello.com>
2024-08-07 14:28:19 +0100

    Add benchmarks
    Add benchmarks project.

---
+++ perf/OpenApi.Extensions.Benchmarks/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Running;
+using MartinCostello.OpenApi;
+
+if (args.SequenceEqual(["--test"]))
+{
+    await using var benchmarks = new OpenApiBenchmarks();
+    await benchmarks.StartServer();
+
+    try
+    {
+        _ = await benchmarks.GetOpenApiDocumentJson();
+        _ = await benchmarks.GetOpenApiDocumentYaml();
+    }
+    finally
+    {
+        await benchmarks.StopServer();
+    }
+}
+else
+{
+    BenchmarkRunner.Run<OpenApiBenchmarks>(args: args);
+}
