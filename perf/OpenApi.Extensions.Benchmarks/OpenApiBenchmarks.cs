054eaecd362a4f32edb5f71b80fd48c294b0a5d5
martincostello <martin@martincostello.com>
2024-08-07 14:28:19 +0100

    Add benchmarks
    Add benchmarks project.

---
+++ perf/OpenApi.Extensions.Benchmarks/OpenApiBenchmarks.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TodoApp;
+
+namespace MartinCostello.OpenApi;
+
+[EventPipeProfiler(EventPipeProfile.CpuSampling)]
+[MemoryDiagnoser]
+[ShortRunJob]
+public class OpenApiBenchmarks : IAsyncDisposable
+{
+    private WebApplication? _app;
+    private HttpClient? _client;
+    private bool _disposed;
+
+    public OpenApiBenchmarks()
+    {
+        var builder = WebApplication.CreateBuilder([$"--contentRoot={GetContentRoot()}"]);
+
+        builder.Logging.ClearProviders();
+        builder.WebHost.UseUrls("https://127.0.0.1:0");
+
+        builder.AddTodoApp();
+
+        _app = builder.Build();
+        _app.UseTodoApp();
+    }
+
+    [GlobalSetup]
+    public async Task StartServer()
+    {
+        if (_app is { } app)
+        {
+            await app.StartAsync();
+
+            var server = app.Services.GetRequiredService<IServer>();
+            var addresses = server.Features.Get<IServerAddressesFeature>();
+
+            var baseAddress = addresses!.Addresses
+                .Select((p) => new Uri(p))
+                .Last();
+
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+            };
+
+#pragma warning disable CA5400
+            _client = new(handler, disposeHandler: true) { BaseAddress = baseAddress };
+#pragma warning restore CA5400
+        }
+    }
+
+    [GlobalCleanup]
+    public async Task StopServer()
+    {
+        if (_app is { } app)
+        {
+            await app.StopAsync();
+            _app = null;
+        }
+    }
+
+    [Benchmark]
+    public async Task<string> GetOpenApiDocumentJson()
+        => await _client!.GetStringAsync("/openapi/v1.json");
+
+    [Benchmark]
+    public async Task<string> GetOpenApiDocumentYaml()
+        => await _client!.GetStringAsync("/openapi/v1.yaml");
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+
+        if (!_disposed)
+        {
+            _client?.Dispose();
+
+            if (_app is not null)
+            {
+                await _app.DisposeAsync();
+            }
+        }
+
+        _disposed = true;
+    }
+
+    private static string GetContentRoot()
+    {
+        string contentRoot = string.Empty;
+        var directoryInfo = new DirectoryInfo(Path.GetDirectoryName(typeof(OpenApiBenchmarks).Assembly.Location)!);
+
+        do
+        {
+            string? solutionPath = Directory.EnumerateFiles(directoryInfo.FullName, "TodoApp.sln").FirstOrDefault();
+
+            if (solutionPath is not null)
+            {
+                contentRoot = Path.GetFullPath(Path.Combine(directoryInfo.FullName, "samples", "TodoApp"));
+                break;
+            }
+
+            directoryInfo = directoryInfo.Parent;
+        }
+        while (directoryInfo is not null);
+
+        return contentRoot;
+    }
+}
--- samples/TodoApp/Program.cs
+++ samples/TodoApp/Program.cs
@@ -1,117 +1,19 @@
 ﻿// Copyright (c) Martin Costello, 2024. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using MartinCostello.OpenApi;
-using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.WebUtilities;
 using TodoApp;
 
 // Create the default web application builder
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure the Todo repository and associated services
-builder.Services.AddTodoApi();
-
-// Configure OpenAPI documentation for the Todo API
-builder.Services.AddOpenApi(options =>
-{
-    options.AddDocumentTransformer((document, _, _) =>
-    {
-        document.Info.Title = "Todo API";
-        document.Info.Description = "An API for managing Todo items.";
-        document.Info.Version = "v1";
-
-        document.Info.Contact = new()
-        {
-            Name = "martincostello",
-            Url = new("https://github.com/martincostello/openapi-extensions"),
-        };
-
-        document.Info.License = new()
-        {
-            Name = "Apache 2.0",
-            Url = new("https://www.apache.org/licenses/LICENSE-2.0"),
-        };
-
-        return Task.CompletedTask;
-    });
-});
-
-// Configure extensions for OpenAPI
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddOpenApiExtensions(options =>
-{
-    // Always return the server URLs in the OpenAPI document
-    options.AddServerUrls = true;
-
-    // Set a default URL to use for generation of the OpenAPI document using
-    // https://www.nuget.org/packages/Microsoft.Extensions.ApiDescription.Server.
-    options.DefaultServerUrl = "https://localhost:50001";
-
-    // Add examples for OpenAPI operations and components
-    options.AddExamples = true;
-
-    // Add JSON serialization context to use to serialize examples
-    options.SerializationContexts.Add(TodoJsonSerializerContext.Default);
-
-    // Add custom example providers for GUIDs and ProblemDetails
-    options.AddExample<Guid, GuidExampleProvider>();
-    options.AddExample<ProblemDetails, ProblemDetailsExampleProvider>();
-
-    // Configure XML comments for the schemas in the OpenAPI document
-    options.AddXmlComments<Program>();
-
-    // Add a custom transformation for the descriptions in the OpenAPI document
-    options.DescriptionTransformations.Add((p) => p.Replace(" This class cannot be inherited.", string.Empty, StringComparison.Ordinal));
-});
-
-if (string.Equals(builder.Configuration["CODESPACES"], "true", StringComparison.OrdinalIgnoreCase))
-{
-    // When running in GitHub Codespaces, X-Forwarded-Host also needs to be set
-    builder.Services.Configure<ForwardedHeadersOptions>(
-        options => options.ForwardedHeaders |= ForwardedHeaders.XForwardedHost);
-}
+builder.AddTodoApp();
 
 // Create the app
 var app = builder.Build();
 
-// Configure error handling
-if (!app.Environment.IsDevelopment())
-{
-    builder.Services.AddProblemDetails();
-    builder.Services.Configure<ProblemDetailsOptions>((options) =>
-    {
-        options.CustomizeProblemDetails = (context) =>
-        {
-            if (context.Exception is not null)
-            {
-                context.ProblemDetails.Detail = "An internal error occurred.";
-            }
-
-            context.ProblemDetails.Instance = context.HttpContext.Request.Path;
-            context.ProblemDetails.Title = ReasonPhrases.GetReasonPhrase(context.ProblemDetails.Status ?? StatusCodes.Status500InternalServerError);
-        };
-    });
-}
-
-app.UseStatusCodePagesWithReExecute("/error", "?id={0}");
-
-// Require use of HTTPS in production
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-    app.UseHttpsRedirection();
-}
-
-// Add static files for SwaggerUI
-app.UseStaticFiles();
-
-// Add endpoints for OpenAPI
-app.MapOpenApi();
-
-// Add the HTTP endpoints
-app.MapTodoApiRoutes();
+// Use TodoApp middleware and endpoints with the web application
+app.UseTodoApp();
 
 // Run the application
 app.Run();
